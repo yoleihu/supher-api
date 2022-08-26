374c4bf1feab08d7be2d48aa7cf8594912898ff2
Yolanda Ferreira de Souza <75169200+yoleihu@users.noreply.github.com>
2022-08-26 09:54:30 -0300

    adicionando criptografia na senha no post

--- src/guardian/guardian.service.ts
+++ src/guardian/guardian.service.ts
@@ -2,13 +2,18 @@ import { Injectable } from "@nestjs/common";
 import { CreateGuardianDto } from "./dto/create-guardian.dto";
 import { UpdateGuardianDto } from "./dto/update-guardian.dto";
 import { GuardianRepository } from "./repositories/guardian.repository";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class GuardianService {
   constructor(private readonly repository: GuardianRepository) {}
 
   create(createGuardianDto: CreateGuardianDto) {
-    return this.repository.create(createGuardianDto);
+    const newUser = {
+      ...createGuardianDto,
+      password: bcrypt.hashSync(createGuardianDto.password, 8),
+    };
+    return this.repository.create(newUser);
   }
 
   findAll() {
